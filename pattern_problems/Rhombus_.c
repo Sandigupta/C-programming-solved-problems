eb127f09a100426f96abb10e9c44fb81003daa99
Sandigupta <123671335+Sandigupta@users.noreply.github.com>
2023-11-07 22:53:21 +0530

    Add files via upload

---
+++ pattern_problems/Rhombus_.c
@@ -0,0 +1,38 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     printf("Enter the number of row");
+//     scanf("%d",&n);
+//     int m;
+//     printf("Enter the number of cilumbe");
+//     scanf("%d",&m);
+//     for(int i=n;i<=n;i++){
+//         for(int j=1;j<=m-i;j++){
+//             printf("* ");
+//         }
+//         for(int k=1;k<=4;k++){
+//             printf("# ");
+//         }
+//         printf("\n");
+//     }
+// }
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number of row: ");
+    scanf("%d",&n);
+    int m;
+    printf("Enter the number of columbe: ");
+    scanf("%d",&m);
+    for(int i=1;i<=n;i++){
+           for(int j=1;j<=m-i;j++){
+             printf("# " );
+           } 
+           for(int k=1;k<=4;k++){
+              printf("* ");
+           } 
+           printf("\n");
+        }       
+}
